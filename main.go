73883c9ac49cf357c32a6b311f14a6794a4c9859
Gergő Csókás <gergo.csokas@gmail.com>
2024-11-26 17:49:39 +0700

    :construction: [main] Add config loading for main package

--- main.go
+++ main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"gregcsokas.hu/main/config"
+	"log"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if cfg.ServerEnv == "production" {
+		// TODO add production related things here.
+	}
+}
