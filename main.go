d06ade910ca84d02a77ec501a633d4f19357b880
Gergő Csókás <gergo.csokas@gmail.com>
2024-11-26 18:52:06 +0700

    :sparkles: [main] Use newsletter module in `main`

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"gregcsokas.hu/main/config"
 	"gregcsokas.hu/main/db"
 	"gregcsokas.hu/main/modules/auth"
+	"gregcsokas.hu/main/modules/newsletter"
 	"log"
 )
 
@@ -30,11 +31,13 @@ func main() {
 	}
 
 	authModule := auth.NewModule(database)
+	newsletterModule := newsletter.NewModule(database)
 
 	api := server.Group("/api")
 	v1 := api.Group("/v1")
 
 	auth.RegisterRoutes(v1, authModule)
+	newsletter.RegisterRoutes(v1, newsletterModule)
 
 	err = server.Run(":8060")
 	if err != nil {
